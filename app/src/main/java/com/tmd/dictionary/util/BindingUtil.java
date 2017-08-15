05b16a78cfd281ade3a745a42e86f62b882e5c81
duc bet <ducbet1996@gmail.com>
2017-08-15 15:19:54 +0700

    fix

--- app/src/main/java/com/tmd/dictionary/util/BindingUtil.java
+++ app/src/main/java/com/tmd/dictionary/util/BindingUtil.java
@@ -3,11 +3,8 @@ package com.tmd.dictionary.util;
 import android.databinding.BindingAdapter;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
 
-import com.tmd.dictionary.screen.activity.search.SearchActivity;
 import com.tmd.dictionary.screen.activity.search.SearchPagerAdapter;
-import com.tmd.dictionary.screen.activity.search.SearchViewModel;
 
 /**
  * Created by tmd on 15/08/2017.
@@ -22,5 +19,4 @@ public class BindingUtil {
     public static void setPagerAdapter(ViewPager viewPager, SearchPagerAdapter adapter) {
         viewPager.setAdapter(adapter);
     }
-
 }
