269acd4fbf788c15088c669032242e7188b4c23c
duc bet <ducbet1996@gmail.com>
2017-08-15 13:37:46 +0700

    create grammar fragment

---
+++ app/src/main/java/com/tmd/dictionary/screen/fragment/Grammar/GrammarViewModel.java
@@ -0,0 +1,30 @@
+package com.tmd.dictionary.screen.fragment.Grammar;
+
+import com.tmd.dictionary.screen.activity.search.SearchContract;
+
+/**
+ * Exposes the data to be used in the Grammar screen.
+ */
+public class GrammarViewModel implements GrammarContract.ViewModel {
+    private SearchContract.ViewModel mSearchViewModel;
+    private GrammarContract.Presenter mPresenter;
+
+    public GrammarViewModel(SearchContract.ViewModel searchViewModel) {
+        mSearchViewModel = searchViewModel;
+    }
+
+    @Override
+    public void onStart() {
+        mPresenter.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        mPresenter.onStop();
+    }
+
+    @Override
+    public void setPresenter(GrammarContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+}
