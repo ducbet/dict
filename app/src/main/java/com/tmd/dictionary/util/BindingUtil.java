8ef8b4c96d5436dfe37400b88fc04b171b0016eb
duc bet <ducbet1996@gmail.com>
2017-08-15 15:17:29 +0700

    create search view

--- app/src/main/java/com/tmd/dictionary/util/BindingUtil.java
+++ app/src/main/java/com/tmd/dictionary/util/BindingUtil.java
@@ -3,8 +3,11 @@ package com.tmd.dictionary.util;
 import android.databinding.BindingAdapter;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
+import com.tmd.dictionary.screen.activity.search.SearchActivity;
 import com.tmd.dictionary.screen.activity.search.SearchPagerAdapter;
+import com.tmd.dictionary.screen.activity.search.SearchViewModel;
 
 /**
  * Created by tmd on 15/08/2017.
@@ -19,4 +22,5 @@ public class BindingUtil {
     public static void setPagerAdapter(ViewPager viewPager, SearchPagerAdapter adapter) {
         viewPager.setAdapter(adapter);
     }
+
 }
