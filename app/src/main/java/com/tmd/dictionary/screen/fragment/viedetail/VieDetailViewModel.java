500cae94c280fd20babeef8b42cc3d1bfa603b1e
ducbet <ducbet1996@gmail.com>
2017-08-31 20:05:57 +0700

    bind data vie jpn detail fragment

--- app/src/main/java/com/tmd/dictionary/screen/fragment/viedetail/VieDetailViewModel.java
+++ app/src/main/java/com/tmd/dictionary/screen/fragment/viedetail/VieDetailViewModel.java
@@ -1,12 +1,26 @@
 package com.tmd.dictionary.screen.fragment.viedetail;
 
+import com.tmd.dictionary.data.model.VieWord;
+import com.tmd.dictionary.screen.activity.main.MainContract;
+
 /**
  * Exposes the data to be used in the VieDetail screen.
  */
 public class VieDetailViewModel implements VieDetailContract.ViewModel {
+    private MainContract.ViewModel mMainViewModel;
     private VieDetailContract.Presenter mPresenter;
+    private VieWord mVieWord;
 
     public VieDetailViewModel() {
+    }
+
+    public VieDetailViewModel(MainContract.ViewModel mainViewModel, VieWord vieWord) {
+        mMainViewModel = mainViewModel;
+        mVieWord = vieWord;
+    }
+
+    public VieWord getVieWord() {
+        return mVieWord;
     }
 
     @Override
